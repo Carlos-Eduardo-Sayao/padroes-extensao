9929be0ef867ee35fa491e838b39320f00271966
dudus <dudus@pc_de_dudu>
2026-09-17 20:49:44 -0300

    Projeto Decorator

--- src/visitor/Produto.java
+++
@@ -1,6 +0,0 @@
-package visitor;
-
-public interface Produto {
-
-    void aceitar(Visitante visitante);
-}
--- src/visitor/Visitante.java
+++
@@ -1,8 +0,0 @@
-package visitor;
-
-public interface Visitante {
-
-    void visitar(Livro livro);
-
-    void visitar(Eletronico eletronico);
-}
